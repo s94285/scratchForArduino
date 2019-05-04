569be74678265f2fbc9e59bfcfde39316ad01107
happyjohn360 <a22811867@gmail.com>
2019-05-04 19:24:32 +0800

    test for commit

--- ece/ScratchForArduino.java
+++ ece/ScratchForArduino.java
@@ -1,4 +1,21 @@
 package ece;
-
-public class ScratchForArduino {
+import javafx.application.Application ;
+import javafx.fxml.FXMLLoader ;
+import javafx.scene.Parent ;
+import javafx.scene.Scene ;
+import javafx.stage.Stage ;
+public class ScratchForArduino extends  Application{
+    @Override
+    public void start ( Stage stage ) throws Exception {
+        Parent root =
+                FXMLLoader . load ( getClass (). getResource ( "ui.fxml" ));
+        Scene scene = new Scene ( root );
+        stage . setTitle ( "ScratchForArduino" ); // displayed in window's title bar
+        stage . setScene ( scene );
+        stage . show ();
+    }
+    public static void main ( String [] args ) {
+        launch ( args );
+    }
 }
+
