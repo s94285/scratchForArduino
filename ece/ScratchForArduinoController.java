bfc3761c15bc4e24c176fb6480be4584cddad471
s94285 <s94285@msn.com>
2019-05-04 23:42:22 +0800

    merge

--- ece/ScratchForArduinoController.java
+++ ece/ScratchForArduinoController.java
@@ -1,12 +1,14 @@
 package ece;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class ScratchForArduinoController {
-    @FXML private Pane pane;
+    @FXML private ToggleGroup BlockToggleGroup;
+    @FXML private Pane drawingPane;
 
     public void initialize(){
         System.out.println("TEST");
@@ -16,9 +18,9 @@ public class ScratchForArduinoController {
         Head head2 = new Head();
         head2.setLayoutX(100);
         head2.setLayoutY(100);
-        pane.getChildren().add(block1);
-        pane.getChildren().add(head2);
-        pane.getChildren().add(new Rectangle(100,100, Color.BLUE));
+        drawingPane.getChildren().add(block1);
+        drawingPane.getChildren().add(head2);
+        drawingPane.getChildren().add(new Rectangle(100,100, Color.BLUE));
 
     }
 }
