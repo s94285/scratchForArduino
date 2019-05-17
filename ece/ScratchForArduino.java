44c346f92b055ff4948d095c4eef8ed161885478
s94285 <s94285@msn.com>
2019-05-17 16:56:18 +0800

    Add org.JSON library

--- ece/ScratchForArduino.java
+++ ece/ScratchForArduino.java
@@ -4,9 +4,30 @@ import javafx.fxml.FXMLLoader ;
 import javafx.scene.Parent ;
 import javafx.scene.Scene ;
 import javafx.stage.Stage ;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.Scanner;
+
 public class ScratchForArduino extends  Application{
     @Override
     public void start ( Stage stage ) throws Exception {
+        /*
+        URL url = new URL(getClass().getResource("PapacodeModules.s2e.json").toString());
+        // read from the URL
+        Scanner scan = new Scanner(url.openStream());
+        String str = new String();
+        while (scan.hasNext())
+            str += scan.nextLine();
+        scan.close();
+        JSONObject json = new JSONObject(str);
+        System.out.println(json.getString("author"));
+        JSONArray array = json.getJSONArray("blockSpecs");
+        for(Object blocks:array){
+            System.out.println(((JSONArray)blocks).getString(0));
+        }
+        */
         Parent root =
                 FXMLLoader . load ( getClass (). getResource ( "ui.fxml" ));
         Scene scene = new Scene ( root );
--- module-info.java
+++ module-info.java
@@ -2,6 +2,7 @@ module scratchForArduino {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.json;
 
     opens ece;
 }
